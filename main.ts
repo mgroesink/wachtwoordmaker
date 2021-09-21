2db63deecebd069c344bcbeb47be56e835b7bad0
Marcel Roesink <mroesink@rocvantwente.nl>
2021-09-21 17:41:07 +0200

    Shuffle text toegevoegd

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+function shuffleText (originalText: any[]) {
+    shuffledPassword = ""
+    tekstlijst = originalText
+    while (tekstlijst.length > 0) {
+        position = randint(0, tekstlijst.length - 1)
+        shuffledPassword = "" + shuffledPassword + tekstlijst[position]
+        tekstlijst.removeAt(position)
+    }
+    return shuffledPassword
+}
 function randomWachtwoord (cijfers: number, letters: number) {
     wachtwoord = ""
     for (let index = 0; index <= aantalCijfers - 1; index++) {
@@ -19,22 +29,34 @@ function randomLetter () {
 input.onButtonPressed(Button.A, function () {
     aantalLetters += 1
 })
-function shuffleTekst (tekst: string) {
-	
-}
+input.onGesture(Gesture.Shake, function () {
+    lijst = []
+    for (let index = 0; index <= "Hallo".length - 1; index++) {
+        lijst.push("hallo".charAt(index))
+    }
+    resultaat = shuffleText(lijst)
+    basic.showString("" + (resultaat))
+})
 input.onButtonPressed(Button.AB, function () {
     if (aantalCijfers < 2 || aantalLetters < 4 || aantalCijfers + aantalLetters < 8) {
         basic.showIcon(IconNames.Sad)
         music.playTone(131, music.beat(BeatFraction.Breve))
     } else {
-        basic.showString("" + (randomWachtwoord(aantalCijfers, aantalLetters)))
+        randomPassword = randomWachtwoord(aantalCijfers, aantalLetters)
     }
+    basic.showString("" + (randomPassword))
 })
 input.onButtonPressed(Button.B, function () {
     aantalCijfers += 1
 })
+let randomPassword = ""
+let resultaat = ""
+let lijst: string[] = []
 let letter = ""
 let wachtwoord = ""
+let position = 0
+let tekstlijst: any[] = []
+let shuffledPassword = ""
 let aantalLetters = 0
 let aantalCijfers = 0
 aantalCijfers = 0
