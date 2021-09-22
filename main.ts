f4191f35714f4d1192edf6ce389973ac82d5794d
Marcel Roesink <mroesink@rocvantwente.nl>
2021-09-22 06:37:12 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,65 @@
+function getRandomLetter () {
+    if (Math.randomBoolean()) {
+        letter = String.fromCharCode(randint(65, 90))
+    } else {
+        letter = String.fromCharCode(randint(97, 122))
+    }
+    return letter
+}
+input.onButtonPressed(Button.A, function () {
+    numberOfLetters += 1
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+function getRandomPassowrd (numberOfDigits: number, numberOfLetters: number) {
+    password = ""
+    for (let index = 0; index <= numberOfDigits - 1; index++) {
+        password = "" + password + convertToText(randint(0, 9))
+    }
+    for (let index2 = 0; index2 <= numberOfLetters - 1; index2++) {
+        password = "" + password + getRandomLetter()
+    }
+    return password
+}
+input.onButtonPressed(Button.AB, function () {
+    if (numberOfDigits < 2 || numberOfLetters < 4 || numberOfDigits + numberOfLetters < 8) {
+        basic.showIcon(IconNames.Sad)
+        music.playTone(131, music.beat(BeatFraction.Breve))
+    } else {
+        randomPassword = getRandomPassowrd(numberOfDigits, numberOfLetters)
+    }
+    basic.showString("" + (randomPassword))
+})
+input.onButtonPressed(Button.B, function () {
+    numberOfDigits += 1
+})
+function stringToArray (text: string) {
+    textArray = []
+    for (let index3 = 0; index3 <= text.length; index3++) {
+        textArray.push(text.charAt(index3))
+    }
+    return textArray
+}
+function arrayToString (textArray: any[]) {
+    result = ""
+    for (let waarde of textArray) {
+        result = "" + result + waarde
+    }
+    return result
+}
+let result: string[][][][][][][][] = []
+let textArray: string[][][][][][][][][] = []
+let randomPassword: string[][][][][][][][] = []
+let password: string[][][][][][][][] = []
+let letter: string[][][][][][][][] = []
+let numberOfLetters = 0
+let numberOfDigits = 0
+let shuffledPassword = ""
+let tekstlijst: any[] = []
+let position = 0
+let lijst: string[] = []
+let resultaat = ""
 function shuffleText (originalText: any[]) {
     shuffledPassword = ""
     tekstlijst = originalText
@@ -8,56 +70,5 @@ function shuffleText (originalText: any[]) {
     }
     return shuffledPassword
 }
-function randomWachtwoord (cijfers: number, letters: number) {
-    wachtwoord = ""
-    for (let index = 0; index <= aantalCijfers - 1; index++) {
-        wachtwoord = "" + wachtwoord + convertToText(randint(0, 9))
-    }
-    for (let index = 0; index <= aantalLetters - 1; index++) {
-        wachtwoord = "" + wachtwoord + randomLetter()
-    }
-    return wachtwoord
-}
-function randomLetter () {
-    if (Math.randomBoolean()) {
-        letter = String.fromCharCode(randint(65, 90))
-    } else {
-        letter = String.fromCharCode(randint(97, 122))
-    }
-    return letter
-}
-input.onButtonPressed(Button.A, function () {
-    aantalLetters += 1
-})
-input.onGesture(Gesture.Shake, function () {
-    lijst = []
-    for (let index = 0; index <= "Hallo".length - 1; index++) {
-        lijst.push("hallo".charAt(index))
-    }
-    resultaat = shuffleText(lijst)
-    basic.showString("" + (resultaat))
-})
-input.onButtonPressed(Button.AB, function () {
-    if (aantalCijfers < 2 || aantalLetters < 4 || aantalCijfers + aantalLetters < 8) {
-        basic.showIcon(IconNames.Sad)
-        music.playTone(131, music.beat(BeatFraction.Breve))
-    } else {
-        randomPassword = randomWachtwoord(aantalCijfers, aantalLetters)
-    }
-    basic.showString("" + (randomPassword))
-})
-input.onButtonPressed(Button.B, function () {
-    aantalCijfers += 1
-})
-let randomPassword = ""
-let resultaat = ""
-let lijst: string[] = []
-let letter = ""
-let wachtwoord = ""
-let position = 0
-let tekstlijst: any[] = []
-let shuffledPassword = ""
-let aantalLetters = 0
-let aantalCijfers = 0
-aantalCijfers = 0
-aantalLetters = 0
+numberOfDigits = 0
+numberOfLetters = 0
